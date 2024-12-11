5ad033e604d0166c957cb4bceb664a0df21ec518
nnicha123 <nicha_ngamtweerat@hotmail.com>
2024-12-11 15:44:41 +0700

    Added configuration to make requests read-only (allow only GET requests)

---
+++ backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,35 @@
+package com.luv2code.ecommerce.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import com.luv2code.ecommerce.entity.Product;
+import com.luv2code.ecommerce.entity.ProductCategory;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+		// Make it read only by disabling put, post and delete
+		HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+		
+//		Disable HTTP methods for Product: PUT, POST and DELETE for Product
+		config.getExposureConfiguration()
+		.forDomainType(Product.class)
+		.withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+		.withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+	
+	
+//	Disable HTTP methods for Product: PUT, POST and DELETE for ProductCategory
+		config.getExposureConfiguration()
+		.forDomainType(ProductCategory.class)
+		.withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+		.withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+
+	}
+}
